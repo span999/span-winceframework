7c2b5fc1c073ecf8cc526306a0f1313a243e8fed
span.liu@gmail.com <span.liu@gmail.com@73576caf-b2a5-d423-0eb8-556077f83d5b>
2010-05-27 17:01:13 +0000

    [SP] check-in spExeAutoRunner code

--- mywinceframework/go.bat
+++ mywinceframework/go.bat
@@ -1,7 +1,17 @@
 @echo on
 
-cd Exe\spExeCPUUtilization
+goto _EXIT
 
+cd Exe\spExeKeepCPUBusy
 build -c
+cd ..\..
 
+cd Exe\spExeIllegalMemoryAccess
+build -c
+cd ..\..
+
+:_EXIT
+
+cd Exe\spExeAutoRunner
+build -c
 cd ..\..
