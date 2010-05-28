6bdf9969d009f3209e2d67fd0eb9a03d46055169
spanliu@gmail.com <spanliu@gmail.com@73576caf-b2a5-d423-0eb8-556077f83d5b>
2010-05-28 04:06:54 +0000

    [Trunk] add support kackloght control in spLibSysHALIoctl lib.

--- mywinceframework/Inc/spLibSysHALIoctl.h
+++ mywinceframework/Inc/spLibSysHALIoctl.h
@@ -6,5 +6,10 @@ DWORD spLibSysHALIoctl_Init( DWORD dwParam );
 DWORD spLibSysHALIoctl_Deinit( DWORD dwParam );
 DWORD spLibSysHALIoctl_Reboot( void );
 
+///Kenel IO backlight access
+DWORD spLibSysHALIoctl_BacklightSet( DWORD dwValue );
+DWORD spLibSysHALIoctl_BacklightGet( void );
+DWORD spLibSysHALIoctl_BacklightUp( void );
+DWORD spLibSysHALIoctl_BacklightDown( void );
 
 #endif	///#ifndef __SPLIBSYSHALIOCTL_H__
