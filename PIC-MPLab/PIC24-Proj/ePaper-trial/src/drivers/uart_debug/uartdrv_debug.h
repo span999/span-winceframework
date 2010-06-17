c4d08c1a204786afcf2fe386a31aca233796eb3d
span.liu@gmail.com <span.liu@gmail.com@73576caf-b2a5-d423-0eb8-556077f83d5b>
2010-06-17 14:55:46 +0000

    [SP] add debug UART code.

--- PIC-MPLab/PIC24-Proj/ePaper-trial/src/drivers/uart_debug/uartdrv_debug.h
+++ PIC-MPLab/PIC24-Proj/ePaper-trial/src/drivers/uart_debug/uartdrv_debug.h
@@ -2,8 +2,12 @@
 #define __UARTDRV_DEBUG_H__
 
 
+extern xQueueHandle hUARTDRV_DEBUG_drvTxQueue;
+extern xQueueHandle hUARTDRV_DEBUG_drvRxQueue;
+extern xTaskHandle hUARTDRV_DEBUG_drvTask;
 
 
+void* pvUARTDRV_DEBUG_drvInit(void* pvParameter);
 
 
 #endif	///#ifndef __UARTDRV_DEBUG_H__
