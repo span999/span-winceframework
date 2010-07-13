cfc2ebc6f246638965729f0d76b217b095075ddf
spanliu@gmail.com <spanliu@gmail.com@73576caf-b2a5-d423-0eb8-556077f83d5b>
2010-07-13 15:37:47 +0000

    [SP] update PM service for system timeout transitions.

--- trunk/PIC-MPLab/PIC24-Proj/ePaper-trial/src/services/PM/PMserv_if.h
+++ trunk/PIC-MPLab/PIC24-Proj/ePaper-trial/src/services/PM/PMserv_if.h
@@ -16,21 +16,40 @@ Initialor		:	span.liu
 #include "mTypeDef.h"
 #include "..\battery\batteryserv_if.h"
 
+
+
 ///PM command list
 typedef enum {
-    PMCmd_ENABLE = 1,		/*  */
-    PMCmd_DISABLE,			/*  */
-	PMCmd_PAUSE,			/*  */
+    PMCmd_ENABLE = 1,		/* PM command, enable */
+    PMCmd_DISABLE,			/* PM command, disable */
+	PMCmd_PAUSE,			/* PM command, pause */
+	PMCmd_TOUCH,			/* PM command, touch from anything */
+	PMCmd_EVENT,			/* PM command, event */
+	PMCmd_SETSYSSTAT_ON,	/* PM command, set system state to ON */
+	PMCmd_SETSYSSTAT_SAVE,	/* PM command, set system state to SAVE */
+	PMCmd_SETSYSSTAT_OFF,	/* PM command, set system state to OFF */
 } PMCMD;
 
 
+///get the queue handle for command in
 xQueueHandle xMicPMGetCmdQueueHandle();
+///get the queue handle for status out
 xQueueHandle xMicPMGetStatQueueHandle();
 
+
+///main service initial routine
 void* pvPMSERV_ServInit(void* pvParameter);
+
+
+///power management
 xPMSYSSTAT xMicPMGetPwrSt();
-bRET xMicPMSetPwrSt();
+bRET xMicPMSetPwrSt( xPMSYSSTAT xSetPMState );
+
+
+///battery function
 xBATTLEVEL xMicPMGetBattLevel();
+xBATTLEVEL xMicPMGetBattLevelForce();
+
 
 
 #endif	///#ifndef __PMSERV_IF_H__
