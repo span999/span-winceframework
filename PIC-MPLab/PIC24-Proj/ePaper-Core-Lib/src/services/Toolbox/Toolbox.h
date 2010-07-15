45651c81373378eb5e1f605bcd098007709011eb
spanliu@gmail.com <spanliu@gmail.com@73576caf-b2a5-d423-0eb8-556077f83d5b>
2010-07-15 09:38:21 +0000

    [SP] update Toolbox code

---
+++ PIC-MPLab/PIC24-Proj/ePaper-Core-Lib/src/services/Toolbox/Toolbox.h
@@ -0,0 +1,29 @@
+/*
+
+
+
+
+*/
+
+
+#ifndef __TOOLBOX_H__
+#define __TOOLBOX_H__
+
+#include "mFreeRTOSDef.h"
+
+typedef    xQueueHandle            hProtectedData;
+
+
+
+UINT uMicGetRAND( void );
+void vMicSLEEP( UINT uiMs );
+void vMicSLEEPuntil( UINT uiMs );
+
+hProtectedData xMicCreateProtectedData( UINT uiDataSizeInByte );
+BOOL xMicSetProtectedData( hProtectedData hData, void* pDataSet );
+BOOL xMicGetProtectedData( hProtectedData hData, void* pDataGet );
+void xMicDeleteProtectedData( hProtectedData hData );
+
+
+#endif	///#ifndef __TOOLBOX_H__
+
