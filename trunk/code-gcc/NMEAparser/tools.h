4e04f345c51c513bad371cb2d9a8e1d99c0fae85
spanliu@gmail.com <spanliu@gmail.com@73576caf-b2a5-d423-0eb8-556077f83d5b>
2010-07-19 03:33:32 +0000

    [SP] create sub-folder for gcc work. and add NMEAparser code as the first project.

---
+++ trunk/code-gcc/NMEAparser/tools.h
@@ -0,0 +1,24 @@
+/*
+ *
+ *
+ *
+ *
+ *
+ *
+ *
+ *
+ */
+
+#ifndef __TOOLS_H__
+#define __TOOLS_H__
+
+
+#define DEBUGMSG_CONSOLE
+#define DEBUGMSG_FILE
+#define DEBUGMSG_FILENAME	"debugmsg.log"
+
+
+void SPPRINTF( char* pString, ... );
+
+
+#endif	///#ifndef __TOOLS_H__
