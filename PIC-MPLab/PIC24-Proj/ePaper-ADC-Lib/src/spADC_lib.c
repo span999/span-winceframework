f9038032f90a7126b1068637556e9f3249fbaa62
spanliu@gmail.com <spanliu@gmail.com@73576caf-b2a5-d423-0eb8-556077f83d5b>
2010-08-04 01:24:46 +0000

    [SP] Check -in code for ADC Lib "ePaper-ADC-Lib" in ePaper. (works in simplified PM task)

---
+++ PIC-MPLab/PIC24-Proj/ePaper-ADC-Lib/src/spADC_lib.c
@@ -0,0 +1,59 @@
+/*
+
+File			:	spADC_libcore.c		
+Description		:	driver main body file. ADC driver core body.
+Initialor		:	span.liu
+
+
+*/
+
+#define USE_AND_OR /* To enable AND_OR mask setting */
+#include <adc.h>
+
+
+
+
+
+unsigned int spGetADCdata( void )
+{
+	unsigned int uiRet = 0;
+	unsigned char i = 0;
+	unsigned int ADCResult[16];
+	unsigned int ADCResultAvg = 0;
+
+	///TODO: read ADC hardware port
+	
+	CloseADC10();
+
+	/*set adc channel*/
+	///AD1CHS:
+	SetChanADC10( ADC_CH0_POS_SAMPLEA_AN2 );
+
+	/*Configure adc*/
+	OpenADC10( 
+		(ADC_MODULE_OFF | ADC_IDLE_CONTINUE | ADC_FORMAT_INTG | ADC_CLK_AUTO |ADC_AUTO_SAMPLING_ON),	///AD1CON1:
+		(ADC_SCAN_ON | ADC_INTR_16_CONV | ADC_ALT_BUF_OFF | ADC_ALT_INPUT_OFF),							///AD1CON2:
+		(ADC_CONV_CLK_SYSTEM | ADC_SAMPLE_TIME_17 | ADC_CONV_CLK_254Tcy),								///AD1CON3:
+		(~(ENABLE_AN2_DIG | ENABLE_AN4_DIG | ENABLE_AN5_DIG) ),															///ADPCFGL:
+		(ENABLE_AN16_DIG | ENABLE_AN17_DIG),															///ADPCFGH:							///ADPCFGH:
+		(ADC_SCAN_AN2),																					///AD1CSSL:
+		0
+	);
+	
+	EnableADC1;
+
+	while( i < 16 )
+    {
+		ConvertADC10();
+		while( BusySampADC1 ); /*wait till conversion complete*/
+		ADCResult[i] = ReadADC10(i);
+		ADCResultAvg = ADCResultAvg + ADCResult[i];
+		i++;
+	}
+	
+	CloseADC10();
+
+	uiRet = ADCResultAvg / 16;
+	return uiRet;
+}
+
