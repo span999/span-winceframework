b72ff903118de5bf1d54ac4f738f684bf32d8f75
spanliu@gmail.com <spanliu@gmail.com@73576caf-b2a5-d423-0eb8-556077f83d5b>
2010-09-06 15:09:37 +0000

    [SP] update first C# WPF application. (Visual C# express 2010)

--- trunk/Visual-C#/WpfApp01/WpfApp01/MainWindow.xaml.cs
+++ trunk/Visual-C#/WpfApp01/WpfApp01/MainWindow.xaml.cs
@@ -13,7 +13,9 @@ using System.Windows.Navigation;
 using System.Windows.Shapes;
 using System.Net.Mail;  ///for mail
 using System.Net.Security;
-
+using System.Net;   ///TCP
+using System.Net.Sockets;   ///TCP
+using System.Threading; ///TCP
 
 using System.IO;
 ///using System.Text;
@@ -154,7 +156,81 @@ namespace WpfApplication1
             f.Close();
         }
 
+        private void button3_Click(object sender, RoutedEventArgs e)
+        {
+
+            ThreadedTcpSrvr server = new ThreadedTcpSrvr();
+            listBox1.Items.Add("button3 Click");
+        }
+        
 
 
-    }
+        public class ThreadedTcpSrvr
+        {
+            private TcpListener client;
+
+            public ThreadedTcpSrvr()
+            {
+                ///client = new TcpListener(9050);
+                client = new TcpListener(IPAddress.Any, 9050);
+                client.Start();
+
+                ///Console.WriteLine("Waiting for clients...");
+                ///listBox1.Items.Add("tcp: " + "Waiting for clients...");
+                while (true)
+                {
+                    while (!client.Pending())
+                    {
+                        Thread.Sleep(1000);
+                    }
+
+                    ConnectionThread newconnection = new ConnectionThread();
+                    newconnection.threadListener = this.client;
+                    Thread newthread = new Thread(new ThreadStart(newconnection.HandleConnection));
+                    newthread.Start();
+                }
+            }
+        }
+
+        class ConnectionThread
+        {
+            public TcpListener threadListener;
+            private static int connections = 0;
+
+            public void HandleConnection()
+            {
+                int recv;
+                byte[] data = new byte[1024];
+
+                TcpClient client = threadListener.AcceptTcpClient();
+                NetworkStream ns = client.GetStream();
+                connections++;
+                ///Console.WriteLine("New client accepted: {0} active connections", connections);
+                ///listBox1.Items.Add("tcp: " + "Waiting for clients...");
+
+                string welcome = "Welcome to my test server";
+                data = Encoding.ASCII.GetBytes(welcome);
+                ns.Write(data, 0, data.Length);
+
+                while (true)
+                {
+                    data = new byte[1024];
+                    recv = ns.Read(data, 0, data.Length);
+                    if (recv == 0)
+                        break;
+
+                    ns.Write(data, 0, recv);
+                }
+                ns.Close();
+                client.Close();
+                connections--;
+                ///Console.WriteLine("Client disconnected: {0} active connections", connections);
+            }
+        }
+
+
+    }///public partial class MainWindow : Window
+
+
+
 }
