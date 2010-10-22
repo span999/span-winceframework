608cf97f5627030d091066c437ffccab4c88b47f
spanliu@gmail.com <spanliu@gmail.com@73576caf-b2a5-d423-0eb8-556077f83d5b>
2010-10-22 06:05:08 +0000

    [SP] update ADC driver library code. support both ADCv3 ADCv2

--- trunk/PIC-MPLab/PIC24-Proj/ePaper-ADC-Lib/src/spADC_lib.c
+++ trunk/PIC-MPLab/PIC24-Proj/ePaper-ADC-Lib/src/spADC_lib.c
@@ -30,15 +30,30 @@ unsigned int spGetADCdata( void )
 	SetChanADC10( ADC_CH0_POS_SAMPLEA_AN2 );
 
 	/*Configure adc*/
+
+#if defined (adc_v3_1) ||defined (adc_v3_2)
+#warning "build with libary ADCv3 up"
 	OpenADC10( 
 		(ADC_MODULE_OFF | ADC_IDLE_CONTINUE | ADC_FORMAT_INTG | ADC_CLK_AUTO |ADC_AUTO_SAMPLING_ON),	///AD1CON1:
 		(ADC_SCAN_ON | ADC_INTR_16_CONV | ADC_ALT_BUF_OFF | ADC_ALT_INPUT_OFF),							///AD1CON2:
 		(ADC_CONV_CLK_SYSTEM | ADC_SAMPLE_TIME_17 | ADC_CONV_CLK_254Tcy),								///AD1CON3:
-		(~(ENABLE_AN2_DIG | ENABLE_AN4_DIG | ENABLE_AN5_DIG) ),															///ADPCFGL:
+		BG_VREF_ENABLE,																					///ANCFG:
+		(ADC_SCAN_AN2),																					///AD1CSSL:
+		0																								///AD1CSSH:
+	);
+#else
+#warning "build with ADC libary ADCv2 or below"
+	OpenADC10( 
+		(ADC_MODULE_OFF | ADC_IDLE_CONTINUE | ADC_FORMAT_INTG | ADC_CLK_AUTO |ADC_AUTO_SAMPLING_ON),	///AD1CON1:
+		(ADC_SCAN_ON | ADC_INTR_16_CONV | ADC_ALT_BUF_OFF | ADC_ALT_INPUT_OFF),							///AD1CON2:
+		(ADC_CONV_CLK_SYSTEM | ADC_SAMPLE_TIME_17 | ADC_CONV_CLK_254Tcy),								///AD1CON3:
+		(~(ENABLE_AN2_DIG | ENABLE_AN4_DIG | ENABLE_AN5_DIG) ),											///ADPCFGL:
 		(ENABLE_AN16_DIG | ENABLE_AN17_DIG),															///ADPCFGH:							///ADPCFGH:
 		(ADC_SCAN_AN2),																					///AD1CSSL:
-		0
+		0																								///AD1CSSH:
 	);
+#endif
+
 	
 	EnableADC1;
 
