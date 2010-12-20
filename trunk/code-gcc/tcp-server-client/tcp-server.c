b63aae49443fdefa86a8c6c232e80819dc4bb2fa
spanliu@gmail.com <spanliu@gmail.com@73576caf-b2a5-d423-0eb8-556077f83d5b>
2010-12-20 16:09:04 +0000

    [SP] add test code for tcp-ip in gcc.

---
+++ trunk/code-gcc/tcp-server-client/tcp-server.c
@@ -0,0 +1,23 @@
+/*
+ *
+ *
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+/*
+int ServSock = 0;
+*/
+
+
+int tcpServer( int iPara )
+{
+	int iRet = 0;
+	int ServSock = 0;
+
+
+	return iRet;
+}
+
