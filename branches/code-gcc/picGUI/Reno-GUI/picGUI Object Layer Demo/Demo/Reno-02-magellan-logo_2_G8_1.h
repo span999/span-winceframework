aec03383175908f5cda0883ea25d987714deb42c
spanliu@gmail.com <spanliu@gmail.com@73576caf-b2a5-d423-0eb8-556077f83d5b>
2011-01-10 07:29:06 +0000

    [SP] add BMP C binary code.

---
+++ branches/code-gcc/picGUI/Reno-GUI/picGUI Object Layer Demo/Demo/Reno-02-magellan-logo_2_G8_1.h
@@ -0,0 +1,5 @@
+#ifndef RENO-02-MAGELLAN-LOGO_2_G8_1_H
+#define  RENO-02-MAGELLAN-LOGO_2_G8_1_H
+#define Reno-02-magellan-logo_2_G8_1_SIZE     3118
+extern BITMAP_FLASH Reno-02-magellan-logo_2_G8_1;
+#endif
