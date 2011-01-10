db68f1b4702d0dbd5e691ceb0e481f6ca7d2440d
spanliu@gmail.com <spanliu@gmail.com@73576caf-b2a5-d423-0eb8-556077f83d5b>
2011-01-10 08:46:32 +0000

    [SP] modify magellan log BMP C binary code.

--- branches/code-gcc/picGUI/Reno-GUI/picGUI Object Layer Demo/Demo/Reno-02-magellan-logo_1.h
+++ branches/code-gcc/picGUI/Reno-GUI/picGUI Object Layer Demo/Demo/Reno-02-magellan-logo_1.h
@@ -1,5 +1,5 @@
 #ifndef _RENO_02_MAGELLAN_LOGO_1_H_
 #define  _RENO_02_MAGELLAN_LOGO_1_H_
 #define Reno-02-magellan-logo_1_SIZE     5206
-extern BITMAP_FLASH Reno-02-magellan-logo_1;
+extern BITMAP_FLASH Reno_magellan_logo_C16;
 #endif
