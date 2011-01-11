ca41ea3b3016f03c9e6f4c4c1319dada83cad7f2
spanliu@gmail.com <spanliu@gmail.com@73576caf-b2a5-d423-0eb8-556077f83d5b>
2011-01-11 03:58:42 +0000

    [SP] add other misc icon BMP C binary code.

---
+++ picGUI/Reno-GUI/picGUI Object Layer Demo/Demo/Reno_misc_icon_C8.h
@@ -0,0 +1,17 @@
+#ifndef RENO_MISC_ICON_C8_H
+#define  RENO_MISC_ICON_C8_H
+
+
+#define Reno_11_key_icon_SIZE     854
+extern BITMAP_FLASH Reno_11_key_icon;
+#define Reno_10_arrow_icon_SIZE     1130
+extern BITMAP_FLASH Reno_10_arrow_icon;
+#define Reno_09_star_icon_SIZE     798
+extern BITMAP_FLASH Reno_09_star_icon;
+#define Reno_08_home_icon_SIZE     1338
+extern BITMAP_FLASH Reno_08_home_icon;
+#define Reno_07_heart_icon_SIZE     1004
+extern BITMAP_FLASH Reno_07_heart_icon;
+
+
+#endif
