cb3dfc839e9c536e6596fccddf1056c6df0fedbe
spanliu@gmail.com <spanliu@gmail.com@73576caf-b2a5-d423-0eb8-556077f83d5b>
2011-01-11 01:23:14 +0000

    [SP] add battery icon BMP C binary code.

---
+++ branches/code-gcc/picGUI/Reno-GUI/picGUI Object Layer Demo/Demo/Reno_battery_icon_C8.h
@@ -0,0 +1,15 @@
+#ifndef _RENO_BATTERY_ICON_H_
+#define  _RENO_BATTERY_ICON_H_
+
+
+#define Reno_06_batteryempty_icon_C8_SIZE     479
+extern BITMAP_FLASH Reno_06_batteryempty_icon_C8;
+#define Reno_05_batterylow_icon_C8_SIZE     500
+extern BITMAP_FLASH Reno_05_batterylow_icon_C8;
+#define Reno_04_batteryhalf_icon_C8_SIZE     500
+extern BITMAP_FLASH Reno_04_batteryhalf_icon_C8;
+#define Reno_03_batteryfull_icon_C8_SIZE     500
+extern BITMAP_FLASH Reno_03_batteryfull_icon_C8;
+
+
+#endif
