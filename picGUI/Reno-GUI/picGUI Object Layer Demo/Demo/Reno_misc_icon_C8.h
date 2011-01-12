5ff4041cbbcceb0d4f795ac22dcba1ec463db655
spanliu@gmail.com <spanliu@gmail.com@73576caf-b2a5-d423-0eb8-556077f83d5b>
2011-01-12 03:28:26 +0000

    [SP] update 2 misc icon BMP C binary code.

--- picGUI/Reno-GUI/picGUI Object Layer Demo/Demo/Reno_misc_icon_C8.h
+++ picGUI/Reno-GUI/picGUI Object Layer Demo/Demo/Reno_misc_icon_C8.h
@@ -1,7 +1,10 @@
 #ifndef RENO_MISC_ICON_C8_H
 #define  RENO_MISC_ICON_C8_H
 
-
+#define Reno_13_star_icon_SIZE     766
+extern BITMAP_FLASH Reno_13_star_icon;
+#define Reno_12_heart_icon_SIZE     972
+extern BITMAP_FLASH Reno_12_heart_icon;
 #define Reno_11_key_icon_SIZE     854
 extern BITMAP_FLASH Reno_11_key_icon;
 #define Reno_10_arrow_icon_SIZE     1130
