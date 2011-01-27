94321403e78596d171a03f1cf2fd9a480012e577
spanliu@gmail.com <spanliu@gmail.com@73576caf-b2a5-d423-0eb8-556077f83d5b>
2011-01-27 16:05:14 +0000

    [SP] check-in Microchip Graphics library ver 2.11.

---
+++ FreeRTOSGUIPCEmulator/StellarisWare/grlib/grlib_set.h
@@ -0,0 +1,52 @@
+//*****************************************************************************
+//
+// grlib.h - Prototypes for the low level primitives provided by the graphics
+//           library.
+//
+// Copyright (c) 2007-2009 Luminary Micro, Inc.  All rights reserved.
+// Software License Agreement
+// 
+// Luminary Micro, Inc. (LMI) is supplying this software for use solely and
+// exclusively on LMI's microcontroller products.
+// 
+// The software is owned by LMI and/or its suppliers, and is protected under
+// applicable copyright laws.  All rights are reserved.  You may not combine
+// this software with "viral" open-source software in order to form a larger
+// program.  Any use in violation of the foregoing restrictions may subject
+// the user to criminal sanctions under applicable laws, as well as to civil
+// liability for the breach of the terms and conditions of this license.
+// 
+// THIS SOFTWARE IS PROVIDED "AS IS".  NO WARRANTIES, WHETHER EXPRESS, IMPLIED
+// OR STATUTORY, INCLUDING, BUT NOT LIMITED TO, IMPLIED WARRANTIES OF
+// MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE APPLY TO THIS SOFTWARE.
+// LMI SHALL NOT, IN ANY CIRCUMSTANCES, BE LIABLE FOR SPECIAL, INCIDENTAL, OR
+// CONSEQUENTIAL DAMAGES, FOR ANY REASON WHATSOEVER.
+// 
+// This is part of revision 4423 of the Stellaris Graphics Library.
+//
+//*****************************************************************************
+
+#ifndef __GRLIB_SET_H__
+#define __GRLIB_SET_H__
+
+#include "grlib/grlib.h"
+#include "grlib/widget.h"
+#include "grlib/canvas.h"
+#include "grlib/checkbox.h"
+#include "grlib/container.h"
+#include "grlib/pushbutton.h"
+#include "grlib/radiobutton.h"
+#include "grlib/slider.h"
+#include "grlib/grlib.h"
+#include "grlib/widget.h"
+#include "grlib/canvas.h"
+#include "grlib/checkbox.h"
+#include "grlib/container.h"
+#include "grlib/pushbutton.h"
+#include "grlib/radiobutton.h"
+#include "grlib/slider.h"
+#include "grlib/pushbutton.h"
+#include "grlib/imgbutton.h"
+#include "grlib/listbox.h"
+
+#endif // __GRLIB_SET_H__
