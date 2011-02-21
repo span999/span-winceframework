a874ad4a738437a30333566e6310478ef620a51b
spanliu@gmail.com <spanliu@gmail.com@73576caf-b2a5-d423-0eb8-556077f83d5b>
2011-02-21 01:05:39 +0000

    [SP] re-sync code for Magellan logo.

---
+++ FreeRTOSGUIPCEmulator-Reno/magellan_logo.h
@@ -0,0 +1,24 @@
+#ifndef __MAGELLAN_LOGO_H__
+#define  __MAGELLAN_LOGO_H__
+
+
+
+#define magellan_logo_08_SIZE     8006
+extern const IMAGE_FLASH magellan_logo_08;
+#define magellan_logo_07_SIZE     8006
+extern const IMAGE_FLASH magellan_logo_07;
+#define magellan_logo_06_SIZE     8006
+extern const IMAGE_FLASH magellan_logo_06;
+#define magellan_logo_05_SIZE     8006
+extern const IMAGE_FLASH magellan_logo_05;
+#define magellan_logo_04_SIZE     8006
+extern const IMAGE_FLASH magellan_logo_04;
+#define magellan_logo_03_SIZE     8006
+extern const IMAGE_FLASH magellan_logo_03;
+#define magellan_logo_02_SIZE     8006
+extern const IMAGE_FLASH magellan_logo_02;
+#define magellan_logo_01_SIZE     8006
+extern const IMAGE_FLASH magellan_logo_01;
+
+
+#endif	/*__MAGELLAN_LOGO_H__*/
