664075c3fc026e6e6df7917db41c58bdfb1f5a21
spanliu@gmail.com <spanliu@gmail.com@73576caf-b2a5-d423-0eb8-556077f83d5b>
2011-03-01 05:25:33 +0000

    [SP] fix display numbers in wrong character.

--- branches/PIC-MPLab/FreeRTOSGUIPCEmulator-Reno/GUIC/inc/FontMonacoU.h
+++ branches/PIC-MPLab/FreeRTOSGUIPCEmulator-Reno/GUIC/inc/FontMonacoU.h
@@ -4,62 +4,66 @@
 
 #if defined(WIN32)
 
-#define Monaco_Normal58U_SIZE     18876
-extern const char Monaco_Normal58U[];
-#define Monaco_Normal52U_SIZE     16968
-extern const char Monaco_Normal52U[];
-#define Monaco_Normal46U_SIZE     15060
-extern const char Monaco_Normal46U[];
-#define Monaco_Normal42U_SIZE     9336
-extern const char Monaco_Normal42U[];
+#define Monaco_Normal56U_SIZE     18240
+extern const char Monaco_Normal56U[];
+#define Monaco_Normal51U_SIZE     16650
+extern const char Monaco_Normal51U[];
+#define Monaco_Normal45U_SIZE     14742
+extern const char Monaco_Normal45U[];
+#define Monaco_Normal41U_SIZE     13470
+extern const char Monaco_Normal41U[];
 #define Monaco_Normal35U_SIZE     7852
 extern const char Monaco_Normal35U[];
-#define Monaco_Normal31U_SIZE     7004
-extern const char Monaco_Normal31U[];
 #define Monaco_Normal30U_SIZE     6792
 extern const char Monaco_Normal30U[];
 #define Monaco_Normal27U_SIZE     6156
 extern const char Monaco_Normal27U[];
-#define Monaco_Normal23U_SIZE     2870
+#define Monaco_Normal26U_SIZE     5944
+extern const char Monaco_Normal26U[];
+#define Monaco_Normal23U_SIZE     5308
 extern const char Monaco_Normal23U[];
 #define Monaco_Normal20U_SIZE     2552
 extern const char Monaco_Normal20U[];
-#define Monaco_Normal18U_SIZE     2340
-extern const char Monaco_Normal18U[];
+#define Monaco_Normal19U_SIZE     2446
+extern const char Monaco_Normal19U[];
+#define Monaco_Normal16U_SIZE     2128
+extern const char Monaco_Normal16U[];
 #define Monaco_Normal15U_SIZE     2022
 extern const char Monaco_Normal15U[];
-#define Monaco_Normal8U_SIZE     1280
-extern const char Monaco_Normal8U[];
+#define Monaco_Normal9U_SIZE     1386
+extern const char Monaco_Normal9U[];
 
 
 #else
 
-#define Monaco_Normal58U_SIZE     18876
-extern FONT_FLASH Monaco_Normal58U;
-#define Monaco_Normal52U_SIZE     16968
-extern FONT_FLASH Monaco_Normal52U;
-#define Monaco_Normal46U_SIZE     15060
-extern FONT_FLASH Monaco_Normal46U;
-#define Monaco_Normal42U_SIZE     9336
-extern FONT_FLASH Monaco_Normal42U;
+#define Monaco_Normal56U_SIZE     18240
+extern FONT_FLASH Monaco_Normal56U;
+#define Monaco_Normal51U_SIZE     16650
+extern FONT_FLASH Monaco_Normal51U;
+#define Monaco_Normal45U_SIZE     14742
+extern FONT_FLASH Monaco_Normal45U;
+#define Monaco_Normal41U_SIZE     13470
+extern FONT_FLASH Monaco_Normal41U;
 #define Monaco_Normal35U_SIZE     7852
 extern FONT_FLASH Monaco_Normal35U;
-#define Monaco_Normal31U_SIZE     7004
-extern FONT_FLASH Monaco_Normal31U;
 #define Monaco_Normal30U_SIZE     6792
 extern FONT_FLASH Monaco_Normal30U;
 #define Monaco_Normal27U_SIZE     6156
 extern FONT_FLASH Monaco_Normal27U;
-#define Monaco_Normal23U_SIZE     2870
+#define Monaco_Normal26U_SIZE     5944
+extern FONT_FLASH Monaco_Normal26U;
+#define Monaco_Normal23U_SIZE     5308
 extern FONT_FLASH Monaco_Normal23U;
 #define Monaco_Normal20U_SIZE     2552
 extern FONT_FLASH Monaco_Normal20U;
-#define Monaco_Normal18U_SIZE     2340
-extern FONT_FLASH Monaco_Normal18U;
+#define Monaco_Normal19U_SIZE     2446
+extern FONT_FLASH Monaco_Normal19U;
+#define Monaco_Normal16U_SIZE     2128
+extern FONT_FLASH Monaco_Normal16U;
 #define Monaco_Normal15U_SIZE     2022
 extern FONT_FLASH Monaco_Normal15U;
-#define Monaco_Normal8U_SIZE     1280
-extern FONT_FLASH Monaco_Normal8U;
+#define Monaco_Normal9U_SIZE     1386
+extern FONT_FLASH Monaco_Normal9U;
 
 #endif
 
