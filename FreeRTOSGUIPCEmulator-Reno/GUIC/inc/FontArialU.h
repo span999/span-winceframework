9eed1a5bd2cafc609e5177f4307e13799fa8b977
spanliu@gmail.com <spanliu@gmail.com@73576caf-b2a5-d423-0eb8-556077f83d5b>
2011-03-16 11:49:32 +0000

    [SP] fixed code missing!!

---
+++ FreeRTOSGUIPCEmulator-Reno/GUIC/inc/FontArialU.h
@@ -0,0 +1,61 @@
+#ifndef __FONTARIALU_H__
+#define  __FONTARIALU_H__
+
+
+#if defined(WIN32)
+
+#define Arial_Bold7U_SIZE     1262
+extern const char Arial_Bold7U[];
+#define Arial_Bold11U_SIZE     1740
+extern const char Arial_Bold11U[];
+#define Arial_Bold14U_SIZE     2158
+extern const char Arial_Bold14U[];
+#define Arial_Normal7U_SIZE     1262
+extern const char Arial_Normal7U[];
+#define Arial_Normal12U_SIZE     1856
+extern const char Arial_Normal12U[];
+#define Arial_Normal14U_SIZE     2130
+extern const char Arial_Normal14U[];
+#define Arial_Normal15U_SIZE     2414
+extern const char Arial_Normal15U[];
+#define Arial_Normal16U_SIZE     2720
+extern const char Arial_Normal16U[];
+#define Arial_Normal18U_SIZE     3794
+extern const char Arial_Normal18U[];
+#define Arial_Normal21U_SIZE     4643
+extern const char Arial_Normal21U[];
+#define Arial_Normal22U_SIZE     4930
+extern const char Arial_Normal22U[];
+#define Arial_Normal24U_SIZE     5360
+extern const char Arial_Normal24U[];
+
+#else
+
+#define Arial_Bold7U_SIZE     1262
+extern FONT_FLASH Arial_Bold7U;
+#define Arial_Bold11U_SIZE     1740
+extern FONT_FLASH Arial_Bold11U;
+#define Arial_Bold14U_SIZE     2158
+extern FONT_FLASH Arial_Bold14U;
+#define Arial_Normal7U_SIZE     1262
+extern FONT_FLASH Arial_Normal7U;
+#define Arial_Normal12U_SIZE     1856
+extern FONT_FLASH Arial_Normal12U;
+#define Arial_Normal14U_SIZE     2130
+extern FONT_FLASH Arial_Normal14U;
+#define Arial_Normal15U_SIZE     2414
+extern FONT_FLASH Arial_Normal15U;
+#define Arial_Normal16U_SIZE     2720
+extern FONT_FLASH Arial_Normal16U;
+#define Arial_Normal18U_SIZE     3794
+extern FONT_FLASH Arial_Normal18U;
+#define Arial_Normal21U_SIZE     4643
+extern FONT_FLASH Arial_Normal21U;
+#define Arial_Normal22U_SIZE     4930
+extern FONT_FLASH Arial_Normal22U;
+#define Arial_Normal24U_SIZE     5360
+extern FONT_FLASH Arial_Normal24U;
+
+#endif
+
+#endif /* __FONTARIALU_H__ */
