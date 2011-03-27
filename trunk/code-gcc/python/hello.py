d841b2dbe8030e078f9a63f74a78d4d05bb31c01
spanliu@gmail.com <spanliu@gmail.com@73576caf-b2a5-d423-0eb8-556077f83d5b>
2011-03-27 14:23:02 +0000

    [SP] add python folder for python study.

---
+++ trunk/code-gcc/python/hello.py
@@ -0,0 +1,39 @@
+#!/usr/bin/env python
+
+"""
+
+script of python
+
+"""
+
+#import common
+# for help function
+#import urllib
+# for str function
+#import string
+# for os function
+#import os
+
+
+def Function1(num): 
+	print num
+
+Function1(3);
+
+ssss = 'good!!'
+
+print ssss
+
+print "Hello, this is called from python!!\r\n"
+
+
+aa = 3
+bb = 4
+cc = aa + bb
+print "a+b"
+print cc
+
+
+
+# help(help)
+
