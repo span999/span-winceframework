018b4d4408d557ed25186f2146dba67f2913ca40
spanliu@gmail.com <spanliu@gmail.com@73576caf-b2a5-d423-0eb8-556077f83d5b>
2011-04-15 07:46:22 +0000

    [SP] add component of float text entry.

---
+++ branches/PIC-MPLab/Micrium-SimSDLXP-Reno/Software/uC-GUI/Application/RenoUICftextentry.h
@@ -0,0 +1,33 @@
+
+/*
+	header of UI frame page list menu functions
+*/
+
+#ifndef _RENOUICFTEXTENTRY_H_
+#define	_RENOUICFTEXTENTRY_H_
+
+#include "RenoGUI.h"
+#include "RenoUIframe.h"
+
+
+/*
+	frame page data of float text entry
+*/
+typedef struct {
+	const GUI_ConstString	sTitle;
+	FRAMEPAGE_HEADER*		pUplevelFrame;
+	FRAMEPAGE_HEADER* 		pNextFrame;
+	WM_HWIN					hWinTextFrame;		///text frame
+	WM_HWIN					hWinInputFrame;		///input frame
+} FP_FTEXTENTRY_HEADER;
+
+
+
+void FloatTextEntryWindow( int iOption );
+void cbFloatTextEntryWindow(WM_MESSAGE* pMsg);
+
+
+#include "RenoUIframepage.h"
+
+
+#endif	/* _RENOUICFTEXTENTRY_H_ */
