524421f44f4813bcd5127573fc59a02c3ade91b6
spanliu@gmail.com <spanliu@gmail.com@73576caf-b2a5-d423-0eb8-556077f83d5b>
2011-04-21 14:30:27 +0000

    [SP] add fonts. change font in listbox.

---
+++ Micrium-SimSDLXP-Reno/Software/uC-GUI/Application/RenoFont.h
@@ -0,0 +1,32 @@
+
+/*
+	header of custom font
+*/
+
+#ifndef _RENOFONT_H_
+#define	_RENOFONT_H_
+
+
+
+extern GUI_CONST_STORAGE GUI_FONT GUI_FontArial32_20pt;
+extern GUI_CONST_STORAGE GUI_FONT GUI_FontArial22_14pt;		///20
+extern GUI_CONST_STORAGE GUI_FONT GUI_FontArial18_12pt;
+extern GUI_CONST_STORAGE GUI_FONT GUI_FontArial17_11pt;		///14
+extern GUI_CONST_STORAGE GUI_FONT GUI_FontArial16_10pt;		///12
+extern GUI_CONST_STORAGE GUI_FONT GUI_FontArial14_8pt;
+
+extern GUI_CONST_STORAGE GUI_FONT GUI_FontMonaco23_12pt;
+extern GUI_CONST_STORAGE GUI_FONT GUI_FontMonaco27_14pt;
+extern GUI_CONST_STORAGE GUI_FONT GUI_FontMonaco30_16pt;
+extern GUI_CONST_STORAGE GUI_FONT GUI_FontMonaco34_18pt;
+extern GUI_CONST_STORAGE GUI_FONT GUI_FontMonaco40_20pt;
+extern GUI_CONST_STORAGE GUI_FONT GUI_FontMonaco42_22pt;
+extern GUI_CONST_STORAGE GUI_FONT GUI_FontMonaco45_24pt;
+extern GUI_CONST_STORAGE GUI_FONT GUI_FontMonaco51_26pt;
+extern GUI_CONST_STORAGE GUI_FONT GUI_FontMonaco53_28pt;
+extern GUI_CONST_STORAGE GUI_FONT GUI_FontMonaco69_36pt;
+extern GUI_CONST_STORAGE GUI_FONT GUI_FontMonaco93_48pt;
+
+
+
+#endif	/* _RENOFONT_H_ */
