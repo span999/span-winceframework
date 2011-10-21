3c9d3563e0b375c7c366c91eb8b88193af12f003
spanliu@gmail.com <spanliu@gmail.com@73576caf-b2a5-d423-0eb8-556077f83d5b>
2011-10-21 08:06:18 +0000

    [SP] add dynamic library test code.

---
+++ trunk/code-gcc/dynamiclib/main.c
@@ -0,0 +1,25 @@
+/*
+*
+*
+*
+*
+*/
+
+
+#include <stdio.h>
+
+
+int main ()
+{
+	int iRet = 0;
+
+	printf("test app start... !!!\r\n");
+
+	staticlib_welcome();
+	sharedlib_welcome();
+
+	return iRet;
+}
+
+
+
