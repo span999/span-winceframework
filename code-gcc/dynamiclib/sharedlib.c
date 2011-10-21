1c551e29c57b6c4108dec30b1087e5847117010f
spanliu@gmail.com <spanliu@gmail.com@73576caf-b2a5-d423-0eb8-556077f83d5b>
2011-10-21 08:06:18 +0000

    [SP] add dynamic library test code.

---
+++ code-gcc/dynamiclib/sharedlib.c
@@ -0,0 +1,19 @@
+/*
+*
+*
+*
+*/
+
+
+#include <stdio.h>
+
+
+
+int sharedlib_welcome()
+{
+	int iRet = 0;
+
+	printf("||| welcome to my SHARED library !!! |||\r\n");
+
+	return iRet;
+}
---
+++ code-gcc/dynamiclib/staticlib.c
@@ -0,0 +1,17 @@
+/*
+*
+*
+*
+*/
+
+#include <stdio.h>
+
+
+int staticlib_welcome()
+{
+	int iRet = 0;
+
+	printf("||| welcome to my STATIC library !!! |||\r\n");
+
+	return iRet;
+}
