4274a94770f0521492a1493ae1a1c78d873aaa73
spanliu@gmail.com <spanliu@gmail.com@73576caf-b2a5-d423-0eb8-556077f83d5b>
2011-10-28 06:52:42 +0000

    [SP] update for i2c bus access.

--- trunk/Android-Java/fullhello-jni/jni/libsIO-common.c
+++ trunk/Android-Java/fullhello-jni/jni/libsIO-common.c
@@ -7,9 +7,14 @@
 
 
 #include <stdio.h>
+///#include <linux/i2c-dev.h>
+#include <sys/ioctl.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
 
-
-
+///#include <linux/i2c-dev.h>
+#define I2C_SLAVE	0x0703	/* Use this slave address */
 
 #define X86CPUINFOFILE		"/proc/cpuinfo"
 #define X86MEMINFOFILE		"/proc/meminfo"
@@ -112,18 +117,100 @@ static int libsi2cdevsearch( const char *filename, int iMaxNum )
 {
 	int iRet = 0;
 	int iLoop = 0;
+	FILE *fp = NULL;
 	char devname[128];
 	char devnum[8];
+	int file = 0;
 
 	printf("searching device with %s upto %d device nodes !!!\r\n", filename, iMaxNum );
 	
 	for( iLoop = 0; iLoop <= iMaxNum; iLoop++ )
 	{
+		///load dev name
 		strcpy(devname, filename);
 		sprintf(devnum,"%d",iLoop);
 		strcat(devname, devnum);
 
 		printf("searching %s !!!\r\n", devname );
+
+#if 0
+		fp = fopen(devname,"r");
+		if( fp == NULL )
+		{
+			printf("Oops!! %s is not available !!!\r\n", devname );
+		}
+		else
+		{
+			printf("Good!! %s is available for access !!!\r\n", devname );
+		}
+
+		if( fp != NULL )	///close only opend
+			fclose(fp);
+#endif
+
+		file = open( devname, O_RDWR );
+		if( file < 0 )
+		{
+			printf("Oops!! %s is not available !!!\r\n", devname );
+		}
+		else
+		{
+			int iLp = 0;
+			///char index = 0;
+
+			printf("Good!! %s is available for access !!!\r\n", devname );
+
+			for( iLp = 0; iLp < 256; iLp++ )
+			{
+				int addr;
+				
+				addr = iLp;
+				///printf("Try %s with chip ID=0x%x !!!\r\n", devname, addr );
+				/// set slave address
+				if( ioctl( file, I2C_SLAVE, addr ) < 0 )
+				{
+					///printf("Oops!! ID=0x%02x on %s is not available !!!\r\n", addr, devname );
+				}
+				else
+				{
+					char buf[10];
+					int readret;
+
+					///printf("Good!! ID=0x%02x on %s is available !!!\r\n", addr, devname );
+					
+					buf[0] = 0x0;	///register 0x0
+					readret = read(file, buf, 1);
+					
+					if( readret != 1)
+						readret = read(file, buf, 1);
+
+					if( readret != 1)
+						readret = read(file, buf, 1);
+
+					if( readret != 1)
+						readret = read(file, buf, 1);
+
+					if( readret != 1) {
+						/* ERROR HANDLING: i2c transaction failed */
+						///printf("Oops!! ID=0x%02x on %s is not readable 0x00(0x%x)!!!\r\n", addr, devname, buf[0] );
+					} else {
+				    	/* buf[0] contains the read byte */
+						printf("Good!! ID=0x%02x on %s is readable 0x00(0x%x)!!!\r\n", addr, devname, buf[0] );
+
+						buf[0] = 0x1;	///register 0x1
+						if(read(file, buf, 1) == 1) {
+							printf("Good!! ID=0x%02x on %s is readable 0x01(0x%x)!!!\r\n", addr, devname, buf[0] );
+						}
+					}
+					
+				}
+			}
+
+			close(file);
+		}
+		
+
+
 	}
 
 	return iRet;
