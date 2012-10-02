41c2a0b4426a42e644f5261b9e1bcabbdc0947c5
spanliu@gmail.com <spanliu@gmail.com@73576caf-b2a5-d423-0eb8-556077f83d5b>
2012-10-02 05:50:56 +0000

    [SP] update svn script for use no stdin.

--- workshop_bin/svn_delall.sh
+++ workshop_bin/svn_delall.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 # add all new to svn
 
-svn st | grep '^\!' | gawk '{ print $2 }' | svn rm --targets -
+###svn st | grep '^\!' | gawk '{ print $2 }' | svn rm --targets -
+
+svn rm `svn st .|grep "^!"|awk '{print $2}'`
 
