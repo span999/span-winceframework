df73faa755bd73bab5686439d5bbe78b955871bf
spanliu@gmail.com <spanliu@gmail.com@73576caf-b2a5-d423-0eb8-556077f83d5b>
2012-10-02 05:50:56 +0000

    [SP] update svn script for use no stdin.

--- trunk/workshop_bin/svn_addall.sh
+++ trunk/workshop_bin/svn_addall.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
 # add all new to svn
 
-svn st | grep '^\?' | gawk '{ print $2 }' | svn add --targets -
+###svn st | grep '^\?' | gawk '{ print $2 }' | svn add --targets -
+
+svn add `svn st .|grep "^?"|awk '{print $2}'`
+
