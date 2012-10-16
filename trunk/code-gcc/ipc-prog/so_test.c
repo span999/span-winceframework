63a2086d5b47690bddf3813d2153ebfd7b4bc9ef
spanliu@gmail.com <spanliu@gmail.com@73576caf-b2a5-d423-0eb8-556077f83d5b>
2012-10-16 09:19:51 +0000

    [SP] add ipc program test code.

---
+++ trunk/code-gcc/ipc-prog/so_test.c
@@ -0,0 +1,36 @@
+/*
+
+
+*/
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <pthread.h>
+
+#include <signal.h>
+#include <sys/time.h>
+
+#include "so_test.h"
+
+
+
+
+
+
+/* 
+Prints a number of characters to stderr, as given by PARAMETERS,
+which is a pointer to a struct char_print_parms. 
+*/
+void* lib_char_print (void* parameters)
+{
+	/* Cast the cookie pointer to the right type. */
+	struct char_print_parms* p = (struct char_print_parms*) parameters;
+	int i;
+
+	for (i = 0; i < p->count; ++i)
+		fputc (p->character, stderr);
+	
+	return NULL;
+}
+
