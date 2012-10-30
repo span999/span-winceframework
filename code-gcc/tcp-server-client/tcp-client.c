d9730d421acde4db876db21282716b0e99fcff34
spanliu@gmail.com <spanliu@gmail.com@73576caf-b2a5-d423-0eb8-556077f83d5b>
2012-10-30 09:47:42 +0000

    [SP] add ipc packet code.

--- code-gcc/tcp-server-client/tcp-client.c
+++ code-gcc/tcp-server-client/tcp-client.c
@@ -146,84 +146,14 @@ int spSocketRecv( char *hostname, int portnum, char *buffer, int buflen )
 
 int main( int argc, char *argv[] )
 {
-#if 0
-	int clntSock = 0;
-	int portno = 0;
-	int ndo = 0;
-	struct sockaddr_in serv_addr;
-	struct hostent *server;
-#endif
 	char buffer[256];
 	int ClientSocketId = 0;
-#if 0    
-	if( argc < 3 ) 
-	{
-		fprintf( stderr, "usage %s hostname port\n\r", argv[0] );
-		exit(0);
-    }
-
-	/*
-	server = gethostbyname( (char *)argv[1], (int)strlen(argv[1]) );
-	*/
-	server = gethostbyname( argv[1] );
-    	
-	if( server == NULL )
-	{
-		fprintf( stderr, "ERROR, no such host\n\r" );
-		exit( 0 );
-	}
-
-	portno = atoi( argv[2] );
-
-
-	/*
-	 * prepare socket address data
-	 * 
-	 * reference link, 
-	 * http://www.umiacs.com/sockaddr_inman.html
-	 */
-	SETZERO( &serv_addr, sizeof(serv_addr) );
-	serv_addr.sin_family = AF_INET;
-/*
-	bcopy((char *)server->h_addr, (char *)&serv_addr.sin_addr.s_addr, server->h_length);
-*/
-#if 0
-	serv_addr.sin_addr.s_addr = inet_addr( "127.0.0.1" );
-#else	
-	inet_aton("127.0.0.1", &serv_addr.sin_addr.s_addr);
-#endif
-	serv_addr.sin_port = htons( portno );
-#endif
 
 
 repeatLoop:
 	spQMSG( "Please enter the message: " );	
 	SETZERO( buffer, 256 );
 	fgets( buffer, 255, stdin );
-#if 0
-	clntSock = socket( AF_INET, SOCK_STREAM, 0 );
-
-	if( clntSock < 0 ) 
-		spERR( "ERROR opening socket" );
-
-	ndo = sizeof( serv_addr );
-	if( connect( clntSock, (struct sockaddr *)&serv_addr, ndo ) < 0 ) 
-		spERR("ERROR connecting");
-
-	ndo = write( clntSock, buffer, strlen(buffer) );
-	if( ndo < 0 ) 
-		spERR( "ERROR writing to socket" );
-
-	SETZERO( buffer, 256 );
-	ndo = read( clntSock, buffer, 255 );
-
-	if( ndo < 0 ) 
- 		spERR( "ERROR reading from socket" );
- 	else
-		spQMSG("SERV: %s\n",buffer);
-	
-	close( clntSock );
-#endif	
 
 #if 0
 	spSocketSend( "127.0.0.1", 77777, buffer );
--- code-gcc/tcp-server-client/toolhelps.h
+++ code-gcc/tcp-server-client/toolhelps.h
@@ -9,10 +9,10 @@
 #define SETZERO(t, s) memset(t, 0, s)
 #define QMSG( X ) fprintf(stderr, X)
 
-extern void spQMSG( char *msgout, ... );
-extern void myerr( char *msg );
-extern void spERR( char *msg );
-extern void millisleep( int milliseconds );
+void spQMSG( char *msgout, ... );
+void myerr( char *msg );
+void spERR( char *msg );
+void millisleep( int milliseconds );
 
 
 
