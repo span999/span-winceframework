c894d930d2d6627ecfb4934412d9ebf04a0aa975
spanliu@gmail.com <spanliu@gmail.com@73576caf-b2a5-d423-0eb8-556077f83d5b>
2012-11-01 03:56:25 +0000

    [SP] modify make file for thread build.

--- trunk/code-gcc/tcp-server-client/sysIPCMGR.c
+++ trunk/code-gcc/tcp-server-client/sysIPCMGR.c
@@ -11,12 +11,13 @@
 #include "tcp-server.h"
 #include "tcp-client.h"
 
-
+static char PROGRAMNAME[] = "sysIPCMGR";
 
 
 int main( int argc, char *argv )
 {
 	int iRet = -1;
+	int iLoop = 0;
 
 
 	/* setup ipc communication routine */
@@ -30,8 +31,10 @@ int main( int argc, char *argv )
 	while( 1 )
 	{
 		sleep( 1 );
+		if( ++iLoop % 5 == 0 )
+			spQMSG( "%s is here ... \n", PROGRAMNAME );
 	}
 
-
+	spQMSG( "Exit %s !!! \n", PROGRAMNAME );
 	return iRet;
 }
--- trunk/code-gcc/tcp-server-client/sysPowerMGR.c
+++ trunk/code-gcc/tcp-server-client/sysPowerMGR.c
@@ -6,16 +6,37 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <stdarg.h>
+#include <pthread.h>
 
 #include "ipcpacket.h"
 #include "sysIPCSRV.h"
 
 
+static char PROGRAMNAME[] = "sysPowerMGR";
+static pthread_t thread_id;
+
+
+void *mainPowerMGR( void *argv )
+{
+	int iLoop = 0;
+
+	while( 1 )
+	{
+		sleep( 1 );
+		if( ++iLoop % 5 == 0 )
+			spQMSG( "%s is here ... \n", "mainPowerMGR" );
+	}
+
+	spQMSG( "Exit %s !!! \n", PROGRAMNAME );
+
+	return;
+}
 
 
 int main( int argc, char *argv )
 {
 	int iRet = -1;
+	int iLoop = 0;
 
 
 	/* setup ipc communication routine */
@@ -23,14 +44,16 @@ int main( int argc, char *argv )
 
 	
 	/* create routine for power manager */
-	
+	pthread_create( &thread_id, NULL, &mainPowerMGR, NULL );
 
 	/* wait until loop end */
 	while( 1 )
 	{
 		sleep( 1 );
+		if( ++iLoop % 5 == 0 )
+			spQMSG( "%s is here ... \n", PROGRAMNAME );
 	}
 
-
+	spQMSG( "Exit %s !!! \n", PROGRAMNAME );
 	return iRet;
 }
