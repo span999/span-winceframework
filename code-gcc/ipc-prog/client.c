b210b1e998d1986d579d1c1fc93738d564f0f9a1
spanliu@gmail.com <spanliu@gmail.com@73576caf-b2a5-d423-0eb8-556077f83d5b>
2012-11-14 07:29:05 +0000

    [SP] update ipc test program host/client for ipcpacket test with power command.

--- code-gcc/ipc-prog/client.c
+++ code-gcc/ipc-prog/client.c
@@ -24,6 +24,8 @@ static int hostpoolReadySEM_ID = 0;
 static int hostpoolAvailableSEM_ID = 0;
 static int hostSHMpool_ID = 0;
 
+
+/* spIPCsend */
 static int SendData( char *pData, unsigned int len )
 {
 	int iRet = -1;
@@ -38,7 +40,10 @@ static int SendData( char *pData, unsigned int len )
 	/* copy data to pool */
 	SharedMemoryIDinit( hostSHMpool_ID, &pPool );
 	if( NULL != pPool )
+	{
 		memcpy( pPool, pData, len );
+		iRet = 0;
+	}
 	else
 		printf("%s:%s: ERROR!! SharedMemoryIDinit return null pointer !\n", __FILE__, __FUNCTION__);
 	SharedMemoryIDdeinit( pPool );
@@ -52,11 +57,62 @@ static int SendData( char *pData, unsigned int len )
 }
 
 
+#define		NAMEDCLINTSHMPATH			("/dev/fb0")
+#define		NAMEDCLIENTSEMAVAPATH		("/dev/loop0")
+
+
+/* spIPCrequest */
+static int RequestData( char *pData, unsigned int len )
+{
+	int iRet = -1;
+	char *pPool = NULL;
+	static int clientpoolAvailableSEM_ID = 0;
+	static int clientSHMpool_ID = 0;
+	struct ipcpacket *pipc;
+	
+	/* create shared memory */
+	clientSHMpool_ID = getNamedSharedMemoryID( NAMEDCLINTSHMPATH, RINGBUFSIZE );
+
+	/* create semaphore for memory pool available */
+	clientpoolAvailableSEM_ID = getNamedSemaphoresID( NAMEDCLIENTSEMAVAPATH );
+	/* init semaphore as pending */
+	SemaphoresIDinitwait( clientpoolAvailableSEM_ID );
+
+	pipc = (struct ipcpacket *)pData;
+	pipc->srcSHMid = clientSHMpool_ID;
+	pipc->srcSEMid = clientpoolAvailableSEM_ID;
+
+	iRet = SendData( pData, len );
+	
+	/* wait response after data out*/
+	printf("%s:%s: wait for response !\n", __FILE__, __FUNCTION__);
+	semLOCK( clientpoolAvailableSEM_ID );
+	
+	
+	/* copy data from pool */
+	SharedMemoryIDinit( clientSHMpool_ID, &pPool );
+	if( NULL != pPool )
+	{
+		memcpy( pData, pPool, len );
+	}
+	else
+		printf("%s:%s: ERROR!! SharedMemoryIDinit return null pointer !\n", __FILE__, __FUNCTION__);
+	SharedMemoryIDdeinit( pPool );
+
+
+	SemaphoresIDdestroy( clientpoolAvailableSEM_ID );
+	SharedMemoryIDdestroy( clientSHMpool_ID );
+
+
+	return iRet;
+}
+
+
+
 int main()
 {
 	int return_value = 0;
 	pthread_t thread_id;
-	char testchar = 'A';
 	int iLoop = 10;
 
 	/* create shared memory */
@@ -76,13 +132,28 @@ int main()
 	printf("The process ID is %d\n", (int)getpid() );
 	printf("The parent process ID is %d\n", (int)getppid() );
 
-	
-	while( iLoop > 0 )
+	if(1)
 	{
-		printf("send data [%c] to shared memory\n", testchar );
-		SendData( &testchar, sizeof(testchar) );
-		testchar = testchar + 1;
-		iLoop--;
+		struct ipcpacket ipc;
+		struct sysPowerCmd *pCmd;
+		int iRet = -1;
+		
+		iLoop = 0;
+		while( iLoop < 10 )
+		{
+			pCmd = (struct sysPowerCmd *)ipc.payload;
+			ipc.payloadnum = sizeof(struct sysPowerCmd);
+			
+			pCmd->cmdID = iLoop;
+			printf("send cmd data [%d] to shared memory\n", iLoop );
+			#if 0
+			SendData( (char *)&ipc, sizeof(struct ipcpacket) );
+			#else
+			iRet = RequestData( (char *)&ipc, sizeof(struct ipcpacket) );
+			printf("get cmd resp [%d]\n", pCmd->rspReturn );
+			#endif
+			iLoop++;
+		}
 	}
 
 
