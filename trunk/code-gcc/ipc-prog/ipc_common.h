66f3a27ac59c81b12ad0f78332dd0cf23b8edc60
spanliu@gmail.com <spanliu@gmail.com@73576caf-b2a5-d423-0eb8-556077f83d5b>
2012-11-14 05:33:00 +0000

    [SP] update ipc library.

---
+++ trunk/code-gcc/ipc-prog/ipc_common.h
@@ -0,0 +1,20 @@
+/*
+
+
+*/
+
+
+
+#ifndef __IPC_COMMON_H__
+#define __IPC_COMMON_H__
+
+
+#define		NAMEDHOSTSHMPATH		("/dev/null")
+#define		NAMEDHOSTSEMAVAPATH		("/dev/zero")
+#define		NAMEDHOSTSeMRDYPATH		("/dev/rtc")
+
+
+
+
+#endif /* __IPC_COMMON_H__ */
+
