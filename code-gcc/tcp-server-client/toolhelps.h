7d64235302d48f4c4e1a01748959bebf6354a985
spanliu@gmail.com <spanliu@gmail.com@73576caf-b2a5-d423-0eb8-556077f83d5b>
2012-11-14 12:38:49 +0000

    [SP] add flag control message in toolhelps.

--- code-gcc/tcp-server-client/toolhelps.h
+++ code-gcc/tcp-server-client/toolhelps.h
@@ -10,10 +10,26 @@
 #define QMSG( X ) fprintf(stderr, X)
 
 void spQMSG( char *msgout, ... );
+void spMSG( int iFlag, char *msgout, ... );
 void myerr( char *msg );
 void spERR( char *msg );
 void millisleep( int milliseconds );
 long spGetTimetick( void );
+
+
+#if 0 /* example */
+to use spMSG() with flags set as below
+
+/* debug flag sets */
+#define	dDBG			0x00001000
+#define	dINFO			0x00000100
+#define	dERR			0x00010000
+/* #define	DBGFSET		(dDBG|dINFO|dERR) */
+#define	DBGFSET		(dINFO|dERR)
+#define	dF(x)		(DBGFSET&x)
+
+#endif
+
 
 
 
