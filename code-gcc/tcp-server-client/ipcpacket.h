09f8123262b738cf21596b2e2e9abd16259a8ecb
spanliu@gmail.com <spanliu@gmail.com@73576caf-b2a5-d423-0eb8-556077f83d5b>
2012-11-15 01:08:18 +0000

    [SP] update ipc packet struct define for shared memory type.

--- code-gcc/tcp-server-client/ipcpacket.h
+++ code-gcc/tcp-server-client/ipcpacket.h
@@ -6,6 +6,7 @@
 #define __IPCPACKET_H__
 
 
+#if 0
 struct ipcpacket
 {
 	char			start[4];		/* packet start sign '5''A''5''A' */
@@ -28,6 +29,25 @@ struct ipcpacket
 	*/ 
 	int				CRC;
 };
+#else
+struct ipcpacket
+{
+	char			start[4];		/* packet start sign '5''A''5''A' */
+	int				userID;			/* user ID */
+	int				ipctype;		/* type of IPC, 0 = tcp/ip, 1 = shared memory */
+	char			srcip[16];		/* 255.255.255.255 */
+	int				srcport;		/* port number */
+	char			tarip[16];		/* 255.255.255.255 */
+	int				tarport;		/* port number */
+	int				srcSHMid;		/* client shared memory pool id */
+	int				srcSEMid;		/* client pool data available semaphore id */
+	unsigned int	serialnum;		/* serial number, same data pack use same serial number. start with '0' */
+	int				packetnum;		/* packet number, used when data zise over packet size. start with '1' */
+	int				payloadnum;		/* size in byte for payload data after */
+	char			payload[255];	/* payload data, max = 255 */
+	int				CRC;
+};
+#endif
 
 
 void spIPCPacketInit( struct ipcpacket *pPack );
