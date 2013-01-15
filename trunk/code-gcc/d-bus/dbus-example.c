ba79f8fffed9ae2ad25946b8a8a49a9d78d1b37f
span.liu@gmail.com <span.liu@gmail.com@73576caf-b2a5-d423-0eb8-556077f83d5b>
2013-01-15 17:12:38 +0000

    [SP] add sysPowerSRV-dbus.

--- trunk/code-gcc/d-bus/dbus-example.c
+++ trunk/code-gcc/d-bus/dbus-example.c
@@ -5,45 +5,54 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+#include "sysPowerSRV-dbus.h"
+
+
+#define DBUSSRV_METHOD_SERVER		"test.method.server"
+#define DBUSSRV_METHOD_OBJECT		"/test/method/Object"
+#define DBUSSRV_METHOD_TYPE			"test.method.Type"
+
+
+
 /**
  * Connect to the DBUS bus and send a broadcast signal
  */
 void sendsignal(char* sigvalue)
 {
-   DBusMessage* msg;
-   DBusMessageIter args;
-   DBusConnection* conn;
-   DBusError err;
-   int ret;
-   dbus_uint32_t serial = 0;
+	DBusMessage* msg;
+	DBusMessageIter args;
+	DBusConnection* conn;
+	DBusError err;
+	int ret;
+	dbus_uint32_t serial = 0;
 
-   printf("Sending signal with value %s\n", sigvalue);
+	printf("Sending signal with value %s\n", sigvalue);
 
-   // initialise the error value
-   dbus_error_init(&err);
+	// initialise the error value
+	dbus_error_init(&err);
 
-   // connect to the DBUS system bus, and check for errors
-   conn = dbus_bus_get(DBUS_BUS_SYSTEM, &err);
-   if (dbus_error_is_set(&err)) { 
-      fprintf(stderr, "Connection Error (%s)\n", err.message); 
-      dbus_error_free(&err); 
-   }
-   if (NULL == conn) { 
-      exit(1); 
-   }
+	// connect to the DBUS system bus, and check for errors
+	conn = dbus_bus_get(DBUS_BUS_SYSTEM, &err);
+	if (dbus_error_is_set(&err)) { 
+		fprintf(stderr, "Connection Error (%s)\n", err.message); 
+		dbus_error_free(&err); 
+	}
+	if (NULL == conn) { 
+		exit(1); 
+	}
 
-   // register our name on the bus, and check for errors
-   ret = dbus_bus_request_name(conn, "test.signal.source", DBUS_NAME_FLAG_REPLACE_EXISTING , &err);
-   if (dbus_error_is_set(&err)) { 
-      fprintf(stderr, "Name Error (%s)\n", err.message); 
-      dbus_error_free(&err); 
-   }
-   if (DBUS_REQUEST_NAME_REPLY_PRIMARY_OWNER != ret) { 
-      exit(1);
-   }
+	// register our name on the bus, and check for errors
+	ret = dbus_bus_request_name(conn, "test.signal.source", DBUS_NAME_FLAG_REPLACE_EXISTING , &err);
+	if (dbus_error_is_set(&err)) { 
+		fprintf(stderr, "Name Error (%s)\n", err.message); 
+		dbus_error_free(&err); 
+	}
+	if (DBUS_REQUEST_NAME_REPLY_PRIMARY_OWNER != ret) { 
+		exit(1);
+	}
 
-   // create a signal & check for errors 
-   msg = dbus_message_new_signal("/test/signal/Object", // object name of the signal
+	// create a signal & check for errors 
+	msg = dbus_message_new_signal("/test/signal/Object", // object name of the signal
                                  "test.signal.Type", // interface name of the signal
                                  "Test"); // name of the signal
    if (NULL == msg) 
@@ -236,25 +245,25 @@ void calls( int iID, int iParam )
 	{
 		case 0:
 	// create a new method call and check for errors
-	msg = dbus_message_new_method_call("test.method.server", // target for the method call
-										"/test/method/Object", // object to call on
-										"test.method.Type", // interface to call on
+	msg = dbus_message_new_method_call(DBUSSRV_METHOD_SERVER, // target for the method call
+										DBUSSRV_METHOD_OBJECT, // object to call on
+										DBUSSRV_METHOD_TYPE, // interface to call on
 										"Method"); // method name
 			break;
 		
 		case 1:
 	// create a new method call and check for errors
-	msg = dbus_message_new_method_call("test.method.server", // target for the method call
-										"/test/method/Object", // object to call on
-										"test.method.Type", // interface to call on
+	msg = dbus_message_new_method_call(DBUSSRV_METHOD_SERVER, // target for the method call
+										DBUSSRV_METHOD_OBJECT, // object to call on
+										DBUSSRV_METHOD_TYPE, // interface to call on
 										"m_CPUcoreUP"); // method name
 			break;
 			
 		case 2:
 	// create a new method call and check for errors
-	msg = dbus_message_new_method_call("test.method.server", // target for the method call
-										"/test/method/Object", // object to call on
-										"test.method.Type", // interface to call on
+	msg = dbus_message_new_method_call(DBUSSRV_METHOD_SERVER, // target for the method call
+										DBUSSRV_METHOD_OBJECT, // object to call on
+										DBUSSRV_METHOD_TYPE, // interface to call on
 										"m_CPUcoreDOWN"); // method name
 			break;
 		
@@ -528,6 +537,8 @@ void receive()
 
 int main(int argc, char** argv)
 {
+	sPSgetCPUActivatedNum();
+	
 	if (2 > argc) {
 		printf ("Syntax: dbus-example [send|receive|listen|query|call] [<param>]\n");
 		return 1;
