17296fa7ceb67006b6eb1497fd990fca86c56ba5
span.liu@gmail.com <span.liu@gmail.com@73576caf-b2a5-d423-0eb8-556077f83d5b>
2013-04-10 06:29:20 +0000

    [SP] add battery info lib.

---
+++ trunk/code-gcc/cpuusage/battutil.c
@@ -0,0 +1,231 @@
+/*
+ * 
+ * 
+
+
+example:
+int main()
+{
+}
+
+
+ *
+ *
+ */
+
+
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include "battutil.h"
+
+
+
+#define	CMD1ADC "cat /sys/class/power_supply/vehicle_battery/current_now"
+#define	CMD1VOLT "cat /sys/class/power_supply/vehicle_battery/voltage_now"
+#define	CMD1PERC "cat /sys/class/power_supply/vehicle_battery/capacity"
+#define	CMD1ONLN "cat /sys/class/power_supply/vehicle_battery/online"
+#define	CMD2ADC "cat /sys/class/power_supply/internal_battery/current_now"
+#define	CMD2VOLT "cat /sys/class/power_supply/internal_battery/voltage_now"
+#define	CMD2PERC "cat /sys/class/power_supply/internal_battery/capacity"
+#define	CMD2STAT "cat /sys/class/power_supply/internal_battery/status"
+
+
+
+static char statOut[4096];
+static char statNum[64];
+
+
+
+///#define	_STATDEBUG_
+
+
+static int getBatteryitem( int iItem )
+{
+	int iRet = (-1);
+	FILE *fp;
+	char *pCh;
+#ifdef _STATDEBUG_
+	int iTmp;
+#endif
+
+	switch( iItem )
+	{
+		case 1:
+			fp = popen( CMD1ADC, "r" );
+			break;
+		case 2:
+			fp = popen( CMD1VOLT, "r" );			
+			break;
+		case 3:
+			fp = popen( CMD1PERC, "r" );			
+			break;
+		case 4:
+			fp = popen( CMD1ONLN, "r" );			
+			break;
+		case 11:
+			fp = popen( CMD2ADC, "r" );
+			break;
+		case 12:
+			fp = popen( CMD2VOLT, "r" );			
+			break;
+		case 13:
+			fp = popen( CMD2PERC, "r" );			
+			break;
+		case 14:
+			fp = popen( CMD2STAT, "r" );			
+			break;
+		default:
+			break;
+	}
+	
+	if( fp == NULL )
+	{
+		printf("Failed on %s !!\n", CMD1ADC);
+		goto _pEXIT;
+	}
+	
+	pCh = statOut;	///set start point
+	while( fgets( pCh, sizeof(statOut)-1, fp ) != NULL )
+	{
+#ifdef _STATDEBUG_
+		///printf("0x%08x:%s", (unsigned int)pCh, statOut );
+		printf("0x%08x:%s", (unsigned int)pCh, pCh );	
+#endif
+		pCh = pCh + strlen(pCh);
+	}
+
+#ifdef _STATDEBUG_	
+	pCh = statOut;	///set start point
+	iTmp = 0;
+	printf("-----------dump from 0x%08x-------------\n", (unsigned int)pCh);
+	while( (iTmp < 4096) && (0 != *(pCh+iTmp)) )
+	{
+		printf("%c", *(pCh+iTmp) );
+		iTmp++;
+	}
+#endif
+
+	pclose( fp );
+	
+_pEXIT:
+	return iRet;
+}
+
+
+int getBatteryinfo( struct BattStatSets *pIn )
+{	
+	int iRet = (-1);
+	char *pCh;
+	struct BattStatNums *pBatt = NULL;
+
+	memset( statOut, 0, sizeof(statOut) );
+
+	pBatt = &(pIn->vehicle);
+
+	/* get battery stat ... ADC */
+	getBatteryitem( 1 );
+	pCh = statOut;	///set start point
+#if 0 /* do it later */
+#endif
+	/* parse battery stat number ... */
+	pBatt->adc = atol(pCh);
+#ifdef _STATDEBUG_	
+	printf("pBatt->adc[%d]\n", pBatt->adc );
+#endif
+
+	/* get battery stat ... voltage */
+	getBatteryitem( 2 );
+	pCh = statOut;	///set start point
+#if 0 /* do it later */
+#endif
+	/* parse battery stat number ... */
+	pBatt->voltage = atol(pCh);
+#ifdef _STATDEBUG_
+	printf("pBatt->voltage[%d]\n", pBatt->voltage );
+#endif
+
+	/* get battery stat ... percentage */
+	getBatteryitem( 3 );
+	pCh = statOut;	///set start point
+#if 0 /* do it later */
+#endif
+	/* parse battery stat number ... */
+	pBatt->percentage = atol(pCh);
+#ifdef _STATDEBUG_
+	printf("pBatt->percentage[%d]\n", pBatt->percentage );
+#endif
+
+	/* get battery stat ... online */
+	getBatteryitem( 4 );
+	pCh = statOut;	///set start point
+#if 0 /* do it later */
+#endif
+	/* parse battery stat number ... */
+	pBatt->online = atol(pCh);
+#ifdef _STATDEBUG_
+	printf("pBatt->online[%d]\n", pBatt->online );
+#endif
+
+
+	pBatt = &(pIn->internal);
+
+	/* get battery stat ... ADC */
+	getBatteryitem( 11 );
+	pCh = statOut;	///set start point
+#if 0 /* do it later */
+#endif
+	/* parse battery stat number ... */
+	pBatt->adc = atol(pCh);
+#ifdef _STATDEBUG_
+	printf("pBatt->adc[%d]\n", pBatt->adc );
+#endif
+
+	/* get battery stat ... voltage */
+	getBatteryitem( 12 );
+	pCh = statOut;	///set start point
+#if 0 /* do it later */
+#endif
+	/* parse battery stat number ... */
+	pBatt->voltage = atol(pCh);
+#ifdef _STATDEBUG_
+	printf("pBatt->voltage[%d]\n", pBatt->voltage );
+#endif
+
+	/* get battery stat ... percentage */
+	getBatteryitem( 13 );
+	pCh = statOut;	///set start point
+#if 0 /* do it later */
+#endif
+	/* parse battery stat number ... */
+	pBatt->percentage = atol(pCh);
+#ifdef _STATDEBUG_
+	printf("pBatt->percentage[%d]\n", pBatt->percentage );
+#endif
+
+	/* get battery stat ... status */
+	getBatteryitem( 14 );
+	pCh = statOut;	///set start point
+#if 0 /* do it later */
+#endif
+	/* parse battery stat number ... */
+	strcpy( pBatt->status, pCh );
+#ifdef _STATDEBUG_
+	printf("pBatt->status[%s]\n", pBatt->status );
+#endif
+
+
+_pEXIT:
+	return iRet;
+}
+
+
+#if 0 /* do it later */
+	if( (*(pCh+0) != 'c') || (*(pCh+1) != 'p') || (*(pCh+2) != 'u') )
+	{
+		printf("It's NOT a /proc/stat cat !![%c][%c][%c][%c]\n", *(pCh+0), *(pCh+1), *(pCh+2), *(pCh+3) );
+		printf("It's NOT a /proc/stat cat !![%s]\n", pCh );
+		goto _pEXIT;
+	} 
+#endif
