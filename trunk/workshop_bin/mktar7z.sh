570fb4f28cc1984f9c5dc2b4ea615f3c963f7d4a
spanliu@gmail.com <spanliu@gmail.com@73576caf-b2a5-d423-0eb8-556077f83d5b>
2013-11-01 02:32:55 +0000

    [SP] update mktar7z.sh

--- trunk/workshop_bin/mktar7z.sh
+++ trunk/workshop_bin/mktar7z.sh
@@ -11,15 +11,21 @@
 # $1 : options
 # $2 : compressed file
 # $3 : source folder
+# $4 : cpu cores
 #
 #echo "==> 7z a -r -t7z -mx=9 -mmt=on -ms=on -v2g $1 $2"
 #7z a -r -t7z -mx=9 -mmt=on -ms=on -v2g $1 $2
 #echo "==> tar cf - $2/ | 7z a -r -t7z -mx=9 -mmt=on -ms=on -v2g -si $1"
 #tar cf - $2/ | 7z a -r -t7z -mx=9 -mmt=on -ms=on -v2g -si $1
+#
+# -t{Type}
+# Type of archive (7z, zip, gzip, bzip2 or tar. 7z format is default) 
+
 
 TOPTIONS=$1
 TFILES=$2
 TFOLDER=$3
+TCPUS=$4
 #TODAY=`date +%Y%m%d%02k%02M%02S`
 TODAY=`date +%Y%m%d%02k%02M`
 
@@ -28,9 +34,20 @@ if [ -z $TFOLDER ]; then
 	TFOLDER=$TFILES
 fi
 
+if [ "" == "$TCPUS" ]; then
+	TCPUS=4
+fi
+
+echo "CPU core=$TCPUS"
 if [ "$TOPTIONS" == "-c" ]; then
-	tar cf - $TFOLDER/ | 7z a -r -t7z -mx=9 -mmt=on -ms=on -v2g -si $TFILES-$TODAY.tar.7z
-#	tar cf - $TFOLDER/ | 7za a -r -t7z -mx=9 -mmt=4 -m0=bzip2 -ms=on -v2g -si $TFILES-$TODAY.tar.7z
+##	the default -m0 of -t7z is LZMA
+#	tar cvf - $TFOLDER/ | 7za a -r -t7z -mx=9 -mmt=$TCPUS -ms=on -v2g -si $TFILES-$TODAY.tar.7z
+#	tar cvf - $TFOLDER/ | 7za a -r -t7z -mx=9 -mmt=$TCPUS -m0=bzip2 -ms=on -v2g -si $TFILES-$TODAY.tar.7z
+##	the default -m0 of -tbzip2 is BZip2
+#	tar cvf - $TFOLDER/ | 7za a -r -tbzip2 -mx=9 -mmt=$TCPUS -ms=on -v2g -si $TFILES-$TODAY.tar.bz2
+	tar cvf - $TFOLDER/ | 7za a -r -t7z -m0=LZMA2 -mx=9 -mmt=$TCPUS -ms=on -v2g -si $TFILES-$TODAY.tar.7z
+##	the default -m0 of -tXZ is LZMA2 
+#	tar cvf - $TFOLDER/ | 7za a -r -tXZ -mx=9 -mmt=$TCPUS -ms=on -v2g -si $TFILES-$TODAY.tar.xz
 fi
 
 if [ "$TOPTIONS" == "-x" ]; then
