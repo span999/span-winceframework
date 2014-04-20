645b0ee9d73a6a33af5697d9d074c881226e1c30
spanliu@gmail.com <spanliu@gmail.com@73576caf-b2a5-d423-0eb8-556077f83d5b>
2014-04-20 08:26:57 +0000

    [SP] add PIC18FxxJ60-DK demo kit project.

---
+++ PIC-MPLab/PIC18_proj/pic18fxxj60_dk.X/sp_typedef.h
@@ -0,0 +1,28 @@
+/******************************************************************************/
+/* System Level #define Macros                                                */
+/******************************************************************************/
+
+/* TODO Define system operating frequency */
+#ifndef __SP_TYPEDEF_H__
+#define __SP_TYPEDEF_H__
+
+typedef             void                        spVOIDt;
+
+typedef             unsigned char               spUINT8t;
+typedef             signed char                 spINT8t;
+
+typedef             unsigned short              spUINT16t;
+typedef             signed short                spINT16t;
+
+typedef             unsigned long               spUINT32t;
+typedef             signed long                 spINT32t;
+
+typedef             float                       spFLOATt;
+
+typedef             unsigned char               spBOOLt;
+#define             spTRUE                      1
+#define             spFALSE                     0
+
+
+
+#endif /* __SP_TYPEDEF_H__ */
