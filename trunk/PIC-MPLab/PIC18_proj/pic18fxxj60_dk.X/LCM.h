cb5ed96adf3962934cfa8b036076fd1accd3d1ab
spanliu@gmail.com <spanliu@gmail.com@73576caf-b2a5-d423-0eb8-556077f83d5b>
2014-04-21 13:49:29 +0000

    [SP] add files for LCM.

---
+++ trunk/PIC-MPLab/PIC18_proj/pic18fxxj60_dk.X/LCM.h
@@ -0,0 +1,16 @@
+/******************************************************************************/
+/* System Level #define Macros                                                */
+/******************************************************************************/
+
+/* TODO Define system operating frequency */
+#ifndef __LCM_H__
+#define __LCM_H__
+
+
+
+spVOIDt LCM_Init(spVOIDt);
+spVOIDt LCM_Send_Byte(spUINT8t nn);
+spVOIDt LCM_Char(spVOIDt);
+
+
+#endif /* __LCM_H__ */
