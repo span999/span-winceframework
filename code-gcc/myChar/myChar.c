5cc4636b96f8b4492975f78651c54d48583b68f0
spdroid <spdroid@gmail.com>
2016-01-21 12:15:15 +0800

    [SP] update my example char module and test source.

--- code-gcc/myChar/myChar.c
+++ code-gcc/myChar/myChar.c
@@ -1,13 +1,9 @@
 /**
- * @file   ebbchar.c
- * @author Derek Molloy
- * @date   7 April 2015
+ * @file   myChar.c
+ * @author 
+ * @date   
  * @version 0.1
- * @brief   An introductory character driver to support the second article of my series on
- * Linux loadable kernel module (LKM) development. This module maps to /dev/ebbchar and
- * comes with a helper C program that can be run in Linux user space to communicate with
- * this the LKM.
- * @see http://www.derekmolloy.ie/ for a full description and follow-up descriptions.
+ * @brief   
  */
 
 #include <linux/init.h>           // Macros used to mark up functions e.g. __init __exit
@@ -16,26 +12,43 @@
 #include <linux/kernel.h>         // Contains types, macros, functions for the kernel
 #include <linux/fs.h>             // Header for the Linux file system support
 #include <asm/uaccess.h>          // Required for the copy to user function
-#define  DEVICE_NAME "mychar"    ///< The device will appear at /dev/ebbchar using this value
+#include <linux/mutex.h>	         /// Required for the mutex functionality
+
+#include <linux/version.h>
+#include <linux/errno.h>
+#include <linux/ioctl.h>
+#include "myChar.h"
+
+#define  DEVICE_NAME "mychar"    ///< The device will appear at /dev/mychar using this value
 #define  CLASS_NAME  "spclass"        ///< The device class -- this is a character device driver
 
+
 MODULE_LICENSE("GPL");            ///< The license type -- this affects available functionality
-MODULE_AUTHOR("Derek Molloy");    ///< The author -- visible when you use modinfo
-MODULE_DESCRIPTION("A simple Linux char driver for the BBB");  ///< The description -- see modinfo
+MODULE_AUTHOR("Some One");    ///< The author -- visible when you use modinfo
+MODULE_DESCRIPTION("A Linux char driver");  ///< The description -- see modinfo
 MODULE_VERSION("0.1");            ///< A version number to inform users
+
+static DEFINE_MUTEX(mychar_mutex);  /// A macro that is used to declare a new mutex that is visible in this file
+                                     /// results in a semaphore variable ebbchar_mutex with value 1 (unlocked)
+                                     /// DEFINE_MUTEX_LOCKED() results in a variable with value 0 (locked)
 
 static int    majorNumber;                  ///< Stores the device number -- determined automatically
 static char   message[256] = {0};           ///< Memory for the string that is passed from userspace
 static short  size_of_message;              ///< Used to remember the size of the string stored
 static int    numberOpens = 0;              ///< Counts the number of times the device is opened
-static struct class*  ebbcharClass  = NULL; ///< The device-driver class struct pointer
-static struct device* ebbcharDevice = NULL; ///< The device-driver device struct pointer
+static struct class*  charClass  = NULL; ///< The device-driver class struct pointer
+static struct device* charDevice = NULL; ///< The device-driver device struct pointer
 
 // The prototype functions for the character driver -- must come before the struct definition
 static int     dev_open(struct inode *, struct file *);
 static int     dev_release(struct inode *, struct file *);
 static ssize_t dev_read(struct file *, char *, size_t, loff_t *);
 static ssize_t dev_write(struct file *, const char *, size_t, loff_t *);
+#if (LINUX_VERSION_CODE < KERNEL_VERSION(2,6,35))
+static int dev_ioctl(struct inode *, struct file *, unsigned int, unsigned long);
+#else
+static long dev_ioctl(struct file *, unsigned int, unsigned long);
+#endif
 
 /** @brief Devices are represented as file structure in the kernel. The file_operations structure from
  *  /linux/fs.h lists the callback functions that you wish to associated with your file operations
@@ -47,6 +60,11 @@ static struct file_operations fops =
    .read = dev_read,
    .write = dev_write,
    .release = dev_release,
+#if (LINUX_VERSION_CODE < KERNEL_VERSION(2,6,35))
+   .ioctl = dev_ioctl,
+#else
+   .unlocked_ioctl = dev_ioctl,
+#endif
 };
 
 /** @brief The LKM initialization function
@@ -55,35 +73,38 @@ static struct file_operations fops =
  *  time and that it can be discarded and its memory freed up after that point.
  *  @return returns 0 if successful
  */
-static int __init ebbchar_init(void){
-   printk(KERN_INFO "EBBChar: Initializing the EBBChar LKM\n");
+static int __init mychar_init(void){
+   printk(KERN_INFO "myChar: Initializing the myChar LKM\n");
 
    // Try to dynamically allocate a major number for the device -- more difficult but worth it
    majorNumber = register_chrdev(0, DEVICE_NAME, &fops);
    if (majorNumber<0){
-      printk(KERN_ALERT "EBBChar failed to register a major number\n");
+      printk(KERN_ALERT "myChar failed to register a major number\n");
       return majorNumber;
    }
-   printk(KERN_INFO "EBBChar: registered correctly with major number %d\n", majorNumber);
+   printk(KERN_INFO "myChar: registered correctly with major number %d\n", majorNumber);
 
    // Register the device class
-   ebbcharClass = class_create(THIS_MODULE, CLASS_NAME);
-   if (IS_ERR(ebbcharClass)){                // Check for error and clean up if there is
+   charClass = class_create(THIS_MODULE, CLASS_NAME);
+   if (IS_ERR(charClass)){                // Check for error and clean up if there is
       unregister_chrdev(majorNumber, DEVICE_NAME);
       printk(KERN_ALERT "Failed to register device class\n");
-      return PTR_ERR(ebbcharClass);          // Correct way to return an error on a pointer
+      return PTR_ERR(charClass);          // Correct way to return an error on a pointer
    }
-   printk(KERN_INFO "EBBChar: device class registered correctly\n");
+   printk(KERN_INFO "myChar: device class registered correctly\n");
 
    // Register the device driver
-   ebbcharDevice = device_create(ebbcharClass, NULL, MKDEV(majorNumber, 0), NULL, DEVICE_NAME);
-   if (IS_ERR(ebbcharDevice)){               // Clean up if there is an error
-      class_destroy(ebbcharClass);           // Repeated code but the alternative is goto statements
+   charDevice = device_create(charClass, NULL, MKDEV(majorNumber, 0), NULL, DEVICE_NAME);
+   if (IS_ERR(charDevice)){               // Clean up if there is an error
+      class_destroy(charClass);           // Repeated code but the alternative is goto statements
       unregister_chrdev(majorNumber, DEVICE_NAME);
       printk(KERN_ALERT "Failed to create the device\n");
-      return PTR_ERR(ebbcharDevice);
+      return PTR_ERR(charDevice);
    }
-   printk(KERN_INFO "EBBChar: device class created correctly\n"); // Made it! device was initialized
+   printk(KERN_INFO "myChar: device class created correctly\n"); // Made it! device was initialized
+   
+   mutex_init(&mychar_mutex);       /// Initialize the mutex lock dynamically at runtime
+   
    return 0;
 }
 
@@ -91,12 +112,13 @@ static int __init ebbchar_init(void){
  *  Similar to the initialization function, it is static. The __exit macro notifies that if this
  *  code is used for a built-in driver (not a LKM) that this function is not required.
  */
-static void __exit ebbchar_exit(void){
-   device_destroy(ebbcharClass, MKDEV(majorNumber, 0));     // remove the device
-   class_unregister(ebbcharClass);                          // unregister the device class
-   class_destroy(ebbcharClass);                             // remove the device class
+static void __exit mychar_exit(void){
+   device_destroy(charClass, MKDEV(majorNumber, 0));     // remove the device
+   class_unregister(charClass);                          // unregister the device class
+   class_destroy(charClass);                             // remove the device class
    unregister_chrdev(majorNumber, DEVICE_NAME);             // unregister the major number
-   printk(KERN_INFO "EBBChar: Goodbye from the LKM!\n");
+   mutex_destroy(&mychar_mutex);        /// destroy the dynamically-allocated mutex
+   printk(KERN_INFO "myChar: Goodbye!\n");
 }
 
 /** @brief The device open function that is called each time the device is opened
@@ -105,8 +127,15 @@ static void __exit ebbchar_exit(void){
  *  @param filep A pointer to a file object (defined in linux/fs.h)
  */
 static int dev_open(struct inode *inodep, struct file *filep){
+   
+   if(!mutex_trylock(&mychar_mutex)){    /// Try to acquire the mutex (i.e., put the lock on/down)
+                                          /// returns 1 if successful and 0 if there is contention
+      printk(KERN_ALERT "myChar: Device in use by another process");
+      return -EBUSY;
+   }
+   
    numberOpens++;
-   printk(KERN_INFO "EBBChar: Device has been opened %d time(s)\n", numberOpens);
+   printk(KERN_INFO "myChar: Device has been opened %d time(s)\n", numberOpens);
    return 0;
 }
 
@@ -124,11 +153,11 @@ static ssize_t dev_read(struct file *filep, char *buffer, size_t len, loff_t *of
    error_count = copy_to_user(buffer, message, size_of_message);
 
    if (error_count==0){            // if true then have success
-      printk(KERN_INFO "EBBChar: Sent %d characters to the user\n", size_of_message);
+      printk(KERN_INFO "myChar: Sent %d characters to the user\n", size_of_message);
       return (size_of_message=0);  // clear the position to the start and return 0
    }
    else {
-      printk(KERN_INFO "EBBChar: Failed to send %d characters to the user\n", error_count);
+      printk(KERN_INFO "myChar: Failed to send %d characters to the user\n", error_count);
       return -EFAULT;              // Failed -- return a bad address message (i.e. -14)
    }
 }
@@ -142,10 +171,54 @@ static ssize_t dev_read(struct file *filep, char *buffer, size_t len, loff_t *of
  *  @param offset The offset if required
  */
 static ssize_t dev_write(struct file *filep, const char *buffer, size_t len, loff_t *offset){
-   sprintf(message, "%s(%d letters)", buffer, len);   // appending received string with its length
+   sprintf(message, "%s(%d letters)", buffer, (int)len);   // appending received string with its length
    size_of_message = strlen(message);                 // store the length of the stored message
-   printk(KERN_INFO "EBBChar: Received %d characters from the user\n", len);
+   printk(KERN_INFO "myChar: Received %d characters from the user\n", (int)len);
    return len;
+}
+
+
+#if (LINUX_VERSION_CODE < KERNEL_VERSION(2,6,35))
+static int dev_ioctl(struct inode *inode, struct file *file, unsigned int ioctl_num, unsigned long ioctl_param) 
+#else
+static long dev_ioctl(struct file *f, unsigned int cmd, unsigned long arg)
+#endif
+{
+	int iRet = (-1);
+	static struct mychar_arg_t prv_arg;
+	
+	printk(KERN_INFO "myChar: dev_ioctl, cmd=%d arg=0x%x\n", cmd, arg);
+	
+	switch (cmd)
+	{
+		case MYCHAR_GET_VALUE:
+			printk(KERN_INFO "myChar: dev_ioctl, MYCHAR_GET_VALUE...arg->data=%u\n", ((struct mychar_arg_t *)arg)->data);
+			if (copy_to_user((struct mychar_arg_t *)arg, &prv_arg, sizeof(struct mychar_arg_t)))
+			{
+				iRet = -EACCES;
+			}
+			break;
+
+		case MYCHAR_CLR_VALUE:
+			printk(KERN_INFO "myChar: dev_ioctl, MYCHAR_CLR_VALUE...\n");
+			prv_arg.data = 0;
+			iRet = 0;
+			break;
+
+		case MYCHAR_SET_VALUE:
+			printk(KERN_INFO "myChar: dev_ioctl, MYCHAR_SET_VALUE...arg->data=%u\n", ((struct mychar_arg_t *)arg)->data);
+			if (copy_from_user(&prv_arg, (struct mychar_arg_t *)arg, sizeof(struct mychar_arg_t)))
+			{
+				iRet = -EACCES;
+			}
+			break;
+
+		default:
+			printk(KERN_INFO "myChar: dev_ioctl, nothing to do...\n");
+			iRet = -EINVAL;
+	}	///switch
+	
+	return iRet;
 }
 
 /** @brief The device release function that is called whenever the device is closed/released by
@@ -154,7 +227,8 @@ static ssize_t dev_write(struct file *filep, const char *buffer, size_t len, lof
  *  @param filep A pointer to a file object (defined in linux/fs.h)
  */
 static int dev_release(struct inode *inodep, struct file *filep){
-   printk(KERN_INFO "EBBChar: Device successfully closed\n");
+   mutex_unlock(&mychar_mutex);          /// Releases the mutex (i.e., the lock goes up)
+   printk(KERN_INFO "myChar: Device successfully closed\n");
    return 0;
 }
 
@@ -162,7 +236,7 @@ static int dev_release(struct inode *inodep, struct file *filep){
  *  identify the initialization function at insertion time and the cleanup function (as
  *  listed above)
  */
-module_init(ebbchar_init);
-module_exit(ebbchar_exit);
+module_init(mychar_init);
+module_exit(mychar_exit);
 
 
--- code-gcc/myChar/myCharTest.c
+++ code-gcc/myChar/myCharTest.c
@@ -1,12 +1,9 @@
 /**
- * @file   testebbchar.c
- * @author Derek Molloy
- * @date   7 April 2015
+ * @file   myCharTest.c
+ * @author 
+ * @date   
  * @version 0.1
- * @brief  A Linux user space program that communicates with the ebbchar.c LKM. It passes a
- * string to the LKM and reads the response from the LKM. For this example to work the device
- * must be called /dev/ebbchar.
- * @see http://www.derekmolloy.ie/ for a full description and follow-up descriptions.
+ * @brief  
 */
 #include<stdio.h>
 #include<stdlib.h>
@@ -14,18 +11,25 @@
 #include<fcntl.h>
 #include<string.h>
  
+#include "myChar.h"
+ 
 #define BUFFER_LENGTH 256               ///< The buffer length (crude but fine)
 static char receive[BUFFER_LENGTH];     ///< The receive buffer from the LKM
  
 int main(){
    int ret, fd;
    char stringToSend[BUFFER_LENGTH];
+   static struct mychar_arg_t ioctl_arg;
+   
+   
    printf("Starting device test code example...\n");
-   fd = open("/dev/ebbchar", O_RDWR);             // Open the device with read/write access
+   fd = open("/dev/mychar", O_RDWR);             // Open the device with read/write access
    if (fd < 0){
       perror("Failed to open the device...");
       return errno;
    }
+   
+   
    printf("Type in a short string to send to the kernel module:\n");
    scanf("%[^\n]%*c", stringToSend);                // Read in a string (with spaces)
    printf("Writing message to the device [%s].\n", stringToSend);
@@ -45,6 +49,52 @@ int main(){
       return errno;
    }
    printf("The received message is: [%s]\n", receive);
+
+   
+   ioctl_arg.data = 0;
+   if (ioctl(fd, MYCHAR_GET_VALUE, &ioctl_arg) == -1)
+   {
+      perror("Failed to ioctl get");
+      printf("get: %u\n", ioctl_arg.data);
+   }
+   else
+   {
+      printf("get: %u\n", ioctl_arg.data);
+   }
+
+   ioctl_arg.data = 2;
+   if (ioctl(fd, MYCHAR_SET_VALUE, &ioctl_arg) == -1)
+   {
+      perror("Failed to ioctl set");
+   }
+   
+   ioctl_arg.data = 9;
+   if (ioctl(fd, MYCHAR_GET_VALUE, &ioctl_arg) == -1)
+   {
+      perror("Failed to ioctl get");
+      printf("get: %u\n", ioctl_arg.data);
+   }
+   else
+   {
+      printf("get: %u\n", ioctl_arg.data);
+   }
+   
+   if (ioctl(fd, MYCHAR_CLR_VALUE) == -1)
+   {
+      perror("Failed to ioctl clr");
+   }
+
+   ioctl_arg.data = 9;
+   if (ioctl(fd, MYCHAR_GET_VALUE, &ioctl_arg) == -1)
+   {
+      perror("Failed to ioctl get");
+      printf("get: %u\n", ioctl_arg.data);
+   }
+   else
+   {
+      printf("get: %u\n", ioctl_arg.data);
+   }
+   
    printf("End of the program\n");
    return 0;
 }
